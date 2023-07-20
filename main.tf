e4eca84a49c523db9b5ae643cb75e62ee09144ff
luciusmeridius <122498234+luciusmeridius@users.noreply.github.com>
2023-07-19 19:49:40 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.0.0"
-    }
-  }
-  required_version = "~> 1.5.2"
-}
-provider "aws" {
-  region = var.aws_region
-}
+#terraform {
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "4.0.0"
+#    }
+#  }
+#  required_version = "~> 1.5.2"
+#}
+#provider "aws" {
+#  region = var.aws_region
+#}
 data "aws_availability_zones" "available" {
   state = "available"
 }
